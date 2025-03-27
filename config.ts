3988e7abcd7d390d59d7110ff670393022a87ddf
TheIhori <86474772+KoTsDrObAsHoM@users.noreply.github.com>
2025-03-27 18:21:21 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -11,8 +11,8 @@ export const window = {
 };
 
 export const api = {
-    ws: 'https://launcher.fusioncraft.ru',
-    web: 'https://launcher.fusioncraft.ru',
+    ws: 'ws://62.122.242.127:1370/ws',
+    web: 'http://62.122.242.127:1370',
     publicKey,
 };
 
